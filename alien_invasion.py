d1e7217f67bc175189799b93283061f708b63290
Shalvin P Shaji <shalvinpshaji@gmail.com>
2022-05-24 18:57:38 +0530

    Initial commit added basic screen ship etc

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+from locale import ALT_DIGITS
+from statistics import quantiles
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+
+    def __init__(self) -> None:
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+    
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            pygame.display.flip()
+    
+if __name__ == "__main__":
+    game = AlienInvasion()
+    game.run_game()
