0cba503bdb1ad5458e30d6381ce76ea72c326ffb
Shalvin P Shaji <shalvinpshaji@gmail.com>
2022-05-24 22:28:39 +0530

    Added alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,12 @@
 from locale import ALT_DIGITS
 from statistics import quantiles
 import sys
+from time import sleep
 import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -18,6 +20,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
 
     
     def run_game(self):
@@ -70,8 +74,33 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
+
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - 2 * alien_width
+        number_aliens_x = available_space_x // (alien_width * 2)
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - 3 * alien_height - ship_height
+        number_rows = available_space_y // (2 * alien_height)
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+            alien = Alien(self)
+            alien_width, alien_height = alien.rect.size
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.y = alien_height + 2 * alien_height * row_number
+            alien.rect.x = alien.x
+            alien.rect.y = alien.y
+            self.aliens.add(alien)
+
+
+
 if __name__ == "__main__":
     game = AlienInvasion()
     game.run_game()
